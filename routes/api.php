1d54056fa6e089f93f7e62816649490fdda0d2bb
BSvetlana <svetlanabisacki@gmail.com>
2018-05-25 20:46:29 +0200

    login in postman

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,10 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/login', 'Auth\LoginController@authenticate');
 
-Route::middleware('api')->get('/movies','MoviesController@index');
-Route::middleware('api')->post('/movies','MoviesController@store');
-Route::middleware('api')->get('/movies/{id}','MoviesController@show');
-Route::middleware('api')->put('/movies/{id}','MoviesController@update');
-Route::middleware('api')->delete('/movies/{id}','MoviesController@destroy');
+Route::middleware('jwt')->get('/movies','MoviesController@index');
+Route::middleware('jwt')->post('/movies','MoviesController@store');
+Route::middleware('jwt')->get('/movies/{id}','MoviesController@show');
+Route::middleware('jwt')->put('/movies/{id}','MoviesController@update');
+Route::middleware('jwt')->delete('/movies/{id}','MoviesController@destroy');
